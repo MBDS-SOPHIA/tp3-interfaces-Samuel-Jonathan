214de404a766919f1706130da34c13bf558d2cd1
jtnsamuel <jtn.samuel@gmail.com>
2025-01-06 10:48:23 +0100

    Affiche un message de félicitation si l'utilisateur gagne

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,46 +3,51 @@ package com.example.diceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-/**
- * This activity allows the user to roll two dice and view the results
- * on the screen.
- */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Find the Button in the layout
+        // Bouton pour lancer les dés
         val rollButton: Button = findViewById(R.id.button)
 
-        // Set a click listener on the button to roll the dice when the user taps the button
+        // Ajout d'un gestionnaire de clic pour lancer les dés
         rollButton.setOnClickListener { rollDice() }
     }
 
     /**
-     * Roll two dice and update the screen with the results.
+     * Lancer deux dés et mettre à jour l'écran avec les résultats.
      */
     private fun rollDice() {
-        // Create two Dice objects with 6 sides and roll them
+        // Crée deux objets Dice avec 6 faces
         val dice1 = Dice(6)
         val dice2 = Dice(6)
+
+        // Lance les dés
         val diceRoll1 = dice1.roll()
         val diceRoll2 = dice2.roll()
 
-        // Update the screen with the dice rolls
+        // Met à jour les résultats des dés sur l'interface utilisateur
         val resultTextView1: TextView = findViewById(R.id.textViewDice1)
         val resultTextView2: TextView = findViewById(R.id.textViewDice2)
 
         resultTextView1.text = diceRoll1.toString()
         resultTextView2.text = diceRoll2.toString()
+
+        // Vérifie si les deux dés affichent le même numéro
+        if (diceRoll1 == diceRoll2) {
+            // Affiche un message de félicitations
+            Toast.makeText(this, "Félicitations ! Vous avez gagné !", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
 /**
- * Dice with a fixed number of sides.
+ * Dé définit avec un nombre fixe de faces.
  */
 class Dice(private val numSides: Int) {
     fun roll(): Int {
